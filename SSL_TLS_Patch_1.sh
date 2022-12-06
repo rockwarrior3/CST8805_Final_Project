9c4f9b623e07ef5341f68b4df2d4c6ee6f18a865
Robert Kerr <56703218+rockwarrior3@users.noreply.github.com>
2022-12-05 20:25:39 -0500

    Update SSL_TLS_Patch_1.sh

--- SSL_TLS_Patch_1.sh
+++ SSL_TLS_Patch_1.sh
@@ -17,31 +17,25 @@ touch /home/student/default-ssl.conf
 echo "<IfModule mod_ssl.c>
 	<VirtualHost _default_:443>
 		ServerAdmin webmaster@localhost
-
 		DocumentRoot /var/www/html
 		ServerName www.heistsless
 		ServerAlias www.heistsless.com
-
 		# Available loglevels: trace8, ..., trace1, debug, info, notice, warn,
 		# error, crit, alert, emerg.
 		# It is also possible to configure the loglevel for particular
 		# modules, e.g.
 		#LogLevel info ssl:warn
-
 		ErrorLog ${APACHE_LOG_DIR}/error.log
 		CustomLog ${APACHE_LOG_DIR}/access.log combined
-
 		# For most configuration files from conf-available/, which are
 		# enabled or disabled at a global level, it is possible to
 		# include a line for only one particular virtual host. For example the
 		# following line enables the CGI configuration for this host only
 		# after it has been globally disabled with "a2disconf".
 		#Include conf-available/serve-cgi-bin.conf
-
 		#   SSL Engine Switch:
 		#   Enable/Disable SSL for this virtual host.
 		SSLEngine on
-
 		#   A self-signed (snakeoil) certificate can be created by installing
 		#   the ssl-cert package. See
 		#   /usr/share/doc/apache2/README.Debian.gz for more info.
@@ -49,7 +43,6 @@ echo "<IfModule mod_ssl.c>
 		#   SSLCertificateFile directive is needed.
 		SSLCertificateFile	/home/student/CST8805_Final_Project-main/CST8805_files/PleaseDontHackMe/WebServer.cer
 		SSLCertificateKeyFile /home/student/CST8805_Final_Project-main/CST8805_files/PleaseDontHackMe/WebServer-Priv.key
-
 		#   Server Certificate Chain:
 		#   Point SSLCertificateChainFile at a file containing the
 		#   concatenation of PEM encoded CA certificates which form the
@@ -58,7 +51,6 @@ echo "<IfModule mod_ssl.c>
 		#   when the CA certificates are directly appended to the server
 		#   certificate for convinience.
 		#SSLCertificateChainFile /etc/apache2/ssl.crt/server-ca.crt
-
 		#   Certificate Authority (CA):
 		#   Set the CA certificate verification path where to find CA
 		#   certificates for client authentication or alternatively one
@@ -68,7 +60,6 @@ echo "<IfModule mod_ssl.c>
 		#		 Makefile to update the hash symlinks after changes.
 		#SSLCACertificatePath /etc/ssl/certs/
 		#SSLCACertificateFile /etc/apache2/ssl.crt/ca-bundle.crt
-
 		#   Certificate Revocation Lists (CRL):
 		#   Set the CA revocation path where to find CA CRLs for client
 		#   authentication or alternatively one huge file containing all
@@ -78,7 +69,6 @@ echo "<IfModule mod_ssl.c>
 		#		 Makefile to update the hash symlinks after changes.
 		#SSLCARevocationPath /etc/apache2/ssl.crl/
 		#SSLCARevocationFile /etc/apache2/ssl.crl/ca-bundle.crl
-
 		#   Client Authentication (Type):
 		#   Client certificate verification type and depth.  Types are
 		#   none, optional, require and optional_no_ca.  Depth is a
@@ -86,11 +76,11 @@ echo "<IfModule mod_ssl.c>
 		#   issuer chain before deciding the certificate is not valid.
 		#SSLVerifyClient require
 		#SSLVerifyDepth  10
-
 		#   SSL Engine Options:
 		#   Set various options for the SSL engine.
 		#   o FakeBasicAuth:
 		#	 Translate the client X.509 into a Basic Authorisation.  This means that
+		#	 the standard Auth/DBMAuth methods can be used for access control.  The
 		#	 user name is the `one line' version of the client's X.509 certificate.
 		#	 Note that no password is obtained from the user. Every entry in the user
 		#	 file needs this password: `xxj31ZMTZzkVA'.
@@ -116,7 +106,6 @@ echo "<IfModule mod_ssl.c>
 		<Directory /usr/lib/cgi-bin>
 				SSLOptions +StdEnvVars
 		</Directory>
-
 		#   SSL Protocol Adjustments:
 		#   The safe and default but still SSL/TLS standard compliant shutdown
 		#   approach is that mod_ssl sends the close notify alert but doesn't wait for
@@ -144,17 +133,13 @@ echo "<IfModule mod_ssl.c>
 		# BrowserMatch "MSIE [2-6]" \
 		#		nokeepalive ssl-unclean-shutdown \
 		#		downgrade-1.0 force-response-1.0
-
 	</VirtualHost>
 </IfModule>
-
-syntax=apache ts=4 sw=4 sts=4 sr noet" >> /home/student/default-ssl.conf
-
+# vim: syntax=apache ts=4 sw=4 sts=4 sr noet" >> /home/student/default-ssl.conf
 sudo chmod 644 /home/student/default-ssl.conf
 sudo chown root:root /home/student/default-ssl.conf
 sudo rm /etc/apache2/sites-available/default-ssl.conf
 sudo mv /home/student/default-ssl.conf /etc/apache2/sites-available
-
 #modify 000-default.conf
 touch /home/student/000-default.conf
 echo "<VirtualHost *:80>
@@ -166,7 +151,6 @@ echo "<VirtualHost *:80>
 	# value is not decisive as it is used as a last resort host regardless.
 	# However, you must set it for any further virtual host explicitly.
 	#ServerName www.example.com
-
 	ServerAdmin webmaster@localhost
 	DocumentRoot /var/www/html
 	ServerName www.heistsless
@@ -176,12 +160,9 @@ echo "<VirtualHost *:80>
 	# It is also possible to configure the loglevel for particular
 	# modules, e.g.
 	#LogLevel info ssl:warn
-
 	Redirect "/" "https://www.heistsless/"
-
 	ErrorLog ${APACHE_LOG_DIR}/error.log
 	CustomLog ${APACHE_LOG_DIR}/access.log combined
-
 	# For most configuration files from conf-available/, which are
 	# enabled or disabled at a global level, it is possible to
 	# include a line for only one particular virtual host. For example the
@@ -189,14 +170,11 @@ echo "<VirtualHost *:80>
 	# after it has been globally disabled with "a2disconf".
 	#Include conf-available/serve-cgi-bin.conf
 </VirtualHost>
-
-syntax=apache ts=4 sw=4 sts=4 sr noet" >> /home/student/000-default.conf
-
+# vim: syntax=apache ts=4 sw=4 sts=4 sr noet" >> /home/student/000-default.conf
 sudo chmod 644 /home/student/000-default.conf
 sudo chown root:root /home/student/000-default.conf
 sudo rm /etc/apache2/sites-available/000-default.conf
 sudo mv /home/student/000-default.conf /etc/apache2/sites-available
-
 sudo a2enmod ssl
 sudo a2enmod headers
 sudo a2enconf ssl-paramas.conf
