a5d981b59b0b39047abf80908161f256dbf99095
Robert Kerr <56703218+rockwarrior3@users.noreply.github.com>
2022-12-05 22:39:53 -0500

    Update SSL_TLS_Patch_1.sh

--- SSL_TLS_Patch_1.sh
+++ SSL_TLS_Patch_1.sh
@@ -1,12 +1,12 @@
 #!/bin/bash 
 
 sudo rm /etc/apache2/conf-available/ssl-params.conf
-sudo chmod 644 ssl-paramas.conf
-sudo chown root:root ssl-paramas.conf
+sudo chmod 644 ssl-params.conf
+sudo chown root:root ssl-params.conf
 sudo mv ssl-params.conf /etc/apache2/conf-available
 
-sudo chmod 644 /home/student/default-ssl.conf
-sudo chown root:root /home/student/default-ssl.conf
+sudo chmod 644 default-ssl.conf
+sudo chown root:root default-ssl.conf
 sudo rm /etc/apache2/sites-available/default-ssl.conf
 sudo mv default-ssl.conf /etc/apache2/sites-available
 
