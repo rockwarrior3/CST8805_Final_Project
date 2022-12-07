c5edd5e98fceb53bba258cfb753a2784d2290f06
kentopping <64541692+kentopping@users.noreply.github.com>
2022-12-06 19:15:32 -0500

    Update upload.php

--- upload.php
+++ upload.php
@@ -3,7 +3,7 @@
 // Original code copied from w3schools.
 // Adapted for CST8805 project by Yvan Perron November 27, 2021
 //Do not change any of the code below this line and up to the "End of do not modify comment below"
-$target_dir = "uploads/";
+$target_dir = "/home/kali/Downloads/";
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 $target_fileSig = $target_dir . basename($_FILES["fileToUploadSig"]["name"]);
 $target_fileX509 = $target_dir . basename($_FILES["fileToUploadX509"]["name"]);
@@ -55,8 +55,27 @@ if  ($uploadOk !== 0) {
 	$cmd = "openssl x509 -inform pem -in $target_fileX509 -pubkey -out $target_filePubKey";
 	$valRes = exec( $cmd );
 	$cmd = "openssl dgst -sha256 -verify $target_filePubKey -signature $target_fileSig $target_file ";
+  
+  // Testing file against CRL
+  $sign = "openssl verify -verbose -crl_check -CRLfile /var/www/html/server_files/CA_CRL.crl.pem -trusted /var/www/html/server_files/CA_Root.cer $target_fileX509";
+  $valRes = shell_exec( $sign );
+  $test = substr($valRes, strpos($valRes, ":") + 1); 
+  $test_string = "OK";
+  $test = trim($test);
+  if ($test == "OK"){
+    echo "&nbsp &nbsp &nbsp CRL Verified";
+  }
+  else{
+    echo "&nbsp &nbsp &nbsp CRL Rejected";
+    
+  }
 	echo " <p style='color:blue; font-size:1.5em;'> Payroll Signature Validation Results via shell_exec() method</p> ";
 	$valRes = shell_exec( $cmd );
 	echo "&nbsp &nbsp &nbsp $valRes";
 }
+// check against CRL
+// input santiization?
+
+
+// create python script to test connections using ssl with curl
 ?>
